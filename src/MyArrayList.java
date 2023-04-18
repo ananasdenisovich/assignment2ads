d99d28899d9bfb6a524352094a6bd5599808f13f
ananasdenisovich <ananasovich2003@mail.ru>
2023-04-19 00:23:58 +0600

    Initial commit

---
+++ src/MyArrayList.java
@@ -0,0 +1,25 @@
+public class MyArrayList<T> {
+    private Object[] arr;
+    private int size = 0;
+    private int capacity = 5;
+    public MyArrayList(){
+        arr = new Object[size];
+    }
+    public T get(int index){
+        return (T) arr[index];
+    }
+    public void add(T newItem){
+        if (size == capacity){
+            increaseCapacity();
+        }
+        arr[size++] = newItem;
+    }
+    private void increaseCapacity(){
+        capacity = (int) (1.5 * capacity);
+        Object[] arr2 = new Object[capacity];
+        for (int i = 0; i < capacity; i++){
+            arr2[i] = arr[i];
+        }
+        arr = arr2;
+    }
+}
