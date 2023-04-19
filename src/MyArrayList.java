94a7d8b97931ac257566006ed3a5faff1cf8ec2e
ananasdenisovich <ananasovich2003@mail.ru>
2023-04-19 22:16:40 +0600

    30th Version added comments

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,22 +1,20 @@
 public class MyArrayList<T> implements MyList {
     private Object[] arr;
-    private int size = 0;
-    private int capacity = 5;
-    private int quantity;
+    private int size = 0; //count of elements
+    private int capacity = 5; //size's max (may change)
     public MyArrayList(){
         this.arr = new Object[capacity];
         this.size = 0;
-        this.quantity = quantity;
     }
     @Override
     public int size() {
         return size;
-    }
+    } //returns size of arraylist according on how many elements we added
 
     @Override
     public boolean contains(Object o) {
 
-        if (o == null) {
+        if (o == null) { //checks if thereis no o and therefore no element (false)
             for (int i = 0; i < size; i++) {
                 if (arr[i] == null) {
                     return false;
@@ -24,8 +22,8 @@ public class MyArrayList<T> implements MyList {
             }
         } else {
             for (int i = 0; i < size; i++) {
-                if (o.equals(arr[i])) {
-                    return true;
+                if (o.equals(arr[i])) { //checks if o is the same as every element of array
+                    return true;//if it is the same, returns true
                 }
             }
         }
@@ -33,37 +31,34 @@ public class MyArrayList<T> implements MyList {
     }
     @Override
     public void add(Object item) {
-        if (size == capacity){
-            increaseCapacity();
+        if (size == capacity){ //checks if size isnt greater that its max possible value
+            increaseCapacity(); //if it is greater, makes it double
         }
-        arr[size++] = item;
+        arr[size++] = item; // assigns an item to index from 0 to last element by increasing size
     }
 
     @Override
     public void add(Object item, int index) {
-        checkIndex(index);
-        if (index < 0 || index > this.size) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        }
+        checkIndex(index); //checks for right index
         if (this.size == this.arr.length) {
-            this.increaseCapacity();
+            this.increaseCapacity(); //increases capability, if size ids equal or bigger
         }
         for (int i = this.size; i > index; i--) {
-            this.arr[i] = this.arr[i-1];
+            this.arr[i] = this.arr[i-1]; //gives value of a previous element, if new one isnt added on last position
         }
-        this.arr[index] = item;
-        this.size++;
+        this.arr[index] = item; //assigns value to new element
+        this.size++; //increases size by 1
     }
 
     @Override
     public boolean removeByObject(Object item) {
         boolean x = false;
         for (int i = 0; i < size; i++) {
-            if (item == arr[i]){
-                remove(i);
-                x = true;
+            if (item == arr[i]){ //finds a given object in array
+                remove(i); //removes it by previous method
+                x = true; //returns true if it is removed
             } else {
-                x = false;
+                x = false;//false if not
             }
         }
         return x;
@@ -71,58 +66,58 @@ public class MyArrayList<T> implements MyList {
     @Override
     public Object remove(int index) {
         checkIndex(index);
-        Object arr3 = arr[index];
+        Object arr3 = arr[index]; //creates new object wuth the value of object of arr1 of the same index
         for (int i = index; i < size - 1; i++) {
-            arr[i] = arr[i + 1];
+            arr[i] = arr[i + 1];//moves every element from last to given one by one
         }
-        arr[size - 1] = "";
-        size--;
+        arr[size - 1] = ""; //makes the last element null, because its value is moved to prev one
+        size--;//decreases the size to keep the count of elements
         return arr3;
     }
 
 
     @Override
     public void clear() {
-        this.arr = new Object[5];
-        this.size = 0;
+        this.arr = new Object[5]; //creates new array with no elements
+        this.size = 0; //return the count of elements to 0
     }
 
     public Object get(int index){
         checkIndex(index);
-        return (Object) arr[index];
+        return (Object) arr[index]; //returns object by its index
     }
 
     @Override
     public int indexOf(Object o) {
-        int x = 0;
-        for (int i = size-1; i >= 0; i--) {
-            if (o == arr[i]){
-                x = i;
+        int index = 0;
+        for (int i = size-1; i >= 0; i--) { //checks all elements, from last to first, so the latter - the first index will be chosen
+            if (o == arr[i]){ //if one of elements is the same as o, it will assign to index its index's value
+                index = i;
             }
         }
-        return x;
+        return index;
     }
 
     @Override
     public int lastIndexOf(Object o) {
         int x = 0;
-        for (int i = 0; i < size; i++) {
-            if (o == arr[i]){
+        for (int i = 0; i < size; i++) {//checks all elements, from first to last, so the last index will be chosen
+            if (o == arr[i]){ //if one of elements is the same as o, it will assign to index its index's value
                 x = i;
             }
         }
         return x;
     }
     public void checkIndex(int index){
-        if(index < 0 || index>=size){
+        if(index < 0 || index>=size){ //checks if index is with the range of 0 and size
             throw new IndexOutOfBoundsException();
         }
     }
     private void increaseCapacity(){
-        capacity = 2 * capacity;
+        capacity = 2 * capacity; //increases capacity 2 times if needed
         Object[] arr2 = new Object[capacity];
         for (int i = 0; i < size; i++){
-            arr2[i] = arr[i];
+            arr2[i] = arr[i]; //creates a new array with new capacity and copys values from prev one
         }
         arr = arr2;
     }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,23 +1,20 @@
-import java.util.Comparator;
-
 public class MyLinkedList<T extends Comparable<T>> implements MyList<T>{
 
     private class Node<E> {
-        public T value;
-        E element;
-        Node<E> next;
-        Node<E> prev;
+        E element; //element of linkedlist
+        Node<E> next; //next node
+        Node<E> prev; //previous node
 
-        Node(E element, Node<E> next, Node<E> prev) {
+        Node(E element, Node<E> next, Node<E> prev) { //new class node with respective attributes
             this.element = element;
             this.next = next;
             this.prev = prev;
         }
     }
 
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
+    private Node<T> head; //first element
+    private Node<T> tail; //last element
+    private int size; //count of elements
 
     public MyLinkedList() {
         head = null;
@@ -26,35 +23,32 @@ public class MyLinkedList<T extends Comparable<T>> implements MyList<T>{
     }
 
     public int size() {
-        return size;
+        return size; //returns current count of elements
     }
 
     public boolean contains(Object o) {
         return indexOf(o) != -1;
-    }
+    } //checks if there is a needed element by indexOf method, where it is already checked
 
     public void add(T item) {
         Node<T> newNode = new Node<>(item, null, tail);
-        if (tail != null) {
+        if (tail != null) { //adds new element instead of tail
             tail.next = newNode;
         } else {
             head = newNode;
         }
         tail = newNode;
-        size++;
+        size++;//increases size as there is 1 new element
     }
 
     public void add(T item, int index) {
         checkIndex(index);
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException();
-        }
         if (index == size) {
-            add(item);
+            add(item); //adds new item
         } else {
             Node<T> current = getNode(index);
-            Node<T> newNode = new Node<>(item, current, current.prev);
-            current.prev.next = newNode;
+            Node<T> newNode = new Node<>(item, current, current.prev); //creates a new Node with increased capacity
+            current.prev.next = newNode;//moves every element after added by 1 to the right
             current.prev = newNode;
             size++;
         }
@@ -63,37 +57,37 @@ public class MyLinkedList<T extends Comparable<T>> implements MyList<T>{
     public boolean removeByObject(T item) {
         Node<T> current = head;
         while (current != null) {
-            if (current.element.equals(item)) {
-                unlink(current);
+            if (current.element.equals(item)) { //finds an object that must be removed
+                undo(current); //removes it
                 return true;
             }
-            current = current.next;
+            current = current.next; //move current to the next element after removed one
         }
         return false;
     }
 
     public T remove(int index) {
         checkIndex(index);
-        Node<T> current = getNode(index);
-        unlink(current);
+        Node<T> current = getNode(index);// makes current a needed element with index
+        undo(current); //removes current
         return current.element;
     }
 
-    private void unlink(Node<T> node) {
+    private void undo(Node<T> node) {
         if (node.prev == null) {
-            head = node.next;
+            head = node.next; //moves every element by 1 to gill the gap of null elements
         } else {
             node.prev.next = node.next;
         }
         if (node.next == null) {
-            tail = node.prev;
+            tail = node.prev; //does the same thing to tail
         } else {
             node.next.prev = node.prev;
         }
-        size--;
+        size--; //decreases size since 1 element is removed
     }
 
-    public void clear() {
+    public void clear() { //clears all values
         head = null;
         tail = null;
         size = 0;
@@ -101,71 +95,47 @@ public class MyLinkedList<T extends Comparable<T>> implements MyList<T>{
 
     public T get(int index) {
         return getNode(index).element;
-    }
+    } //works with getNode() method
 
     private Node<T> getNode(int index) {
         checkIndex(index);
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        if (index < size / 2) {
-            Node<T> current = head;
+        if (index < size / 2) { //applies binary search
+            Node<T> current = head; //if index is less than half of size, starts cchecking from start
             for (int i = 0; i < index; i++) {
-                current = current.next;
+                current = current.next; //checks all elements till the half, until finds the one with needed index
             }
             return current;
         } else {
-            Node<T> current = tail;
-            for (int i = size - 1; i > index; i--) {
+            Node<T> current = tail; //if its on the other half, starts from end
+            for (int i = size - 1; i > index; i--) { //same logic but in other direction
                 current = current.prev;
             }
             return current;
         }
     }
 
-    public int indexOf(Object o) {
-        Node<T> current = head;
+    public int indexOf(Object o) { //same as in array
+        Node<T> current = head; //starts from first element
         int index = 0;
         while (current != null) {
-            if (current.element.equals(o)) {
-                return index;
+            if (current.element.equals(o)) {//checks all elements until equals condition is met
+                return index;//once it is met, returns index of fit element
             }
-            current = current.next;
-            index++;
         }
-        return -1;
+        return -1; //returns -1 if no same element is found
     }
-
     public int lastIndexOf(Object o) {
-        Node<T> current = tail;
+        Node<T> current = tail; //starts from last element
         int index = size - 1;
         while (current != null) {
-            if (current.element.equals(o)) {
-                return index;
+            if (current.element.equals(o)) { //checks all elements until equals condition is met
+                return index; //once it is met, returns index of fit element
             }
 
         }
         return index;
     }
-    public void sort(Comparator<T> comparator) {
-        if (size <= 1) {
-            return; // list is already sorted
-        }
-
-        Node current = head;
-        while (current != null) {
-            Node innerCurrent = current;
-            T value = (T) innerCurrent.value;
-            while (innerCurrent.prev != null && comparator.compare(value, (T) innerCurrent.prev.value) < 0) {
-                innerCurrent.value = innerCurrent.prev.value;
-                innerCurrent = innerCurrent.prev;
-            }
-            innerCurrent.value = value;
-            current = current.next;
-        }
-
-    }
-    public void checkIndex(int index){
+    public void checkIndex(int index){ //same as in array
         if(index < 0 || index>=size){
             throw new IndexOutOfBoundsException();
         }
