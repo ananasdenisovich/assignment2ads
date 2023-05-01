b673d3cd72e19ffc5f0012fc15d42e9bd524032c
ananasdenisovich <ananasovich2003@mail.ru>
2023-05-01 21:03:50 +0600

    45th Version made steps 3 and 4 MyLinkedListQueue class

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,4 +1,7 @@
 import java.util.NoSuchElementException;
 public class MyLinkedListQueue {
-
+    private MyLinkedList linkedQueue;
+    public MyLinkedListQueue(){
+        this.linkedQueue = new MyLinkedList<>();
+    }
 }
