684428d1acf90e676a8d8938225f4622054bbd3f
ananasdenisovich <ananasovich2003@mail.ru>
2023-05-01 12:14:24 +0600

    35th Version added working pop and peek methods

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
         stack.add(1);
         stack.add(2);
         stack.add(3);
-        
         System.out.println(stack.size());
+        stack.pop(stack);
+        displayArray(stack);
+        System.out.println(stack.peek(stack));
 
     }
 
--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -7,5 +7,12 @@ public class MyArrayListStack extends MyArrayList {
     public void push(Object item){
         add(item, 0);
     }
+    public Object pop(MyArrayListStack stack){
+        return stack.remove(stack.size() - 1);
+    }
+    public Object peek(MyArrayListStack stack){
+        return stack.get(stack.size()-1);
+    }
+
 
 }
